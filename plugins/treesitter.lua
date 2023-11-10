0bd3a76febce6d437fca2b82c470ec5e5525d261
Marat Dolotov <marat@it-link45.ru>
2023-11-10 13:08:37 +0500

    treesitter list correct

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-       "lua", "php","html","css", "ts","js","json","tsx"
+       "lua", "php","html","css", "typescript","javascript","json", 
     })
   end,
 }
