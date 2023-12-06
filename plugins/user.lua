932d97102a263d8c44c49c293dda64e7825275ef
Marat Dolotov <marat@it-link45.ru>
2023-12-06 12:57:53 +0500

    added surround

--- plugins/user.lua
+++ plugins/user.lua
@@ -20,4 +20,14 @@ return {
     	end,
     },
     "Bekaboo/deadcolumn.nvim",
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
 }
